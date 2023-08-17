897828dc9767069da9461d4dd5268b6041a39e6d
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-08-17 14:07:10 -0400

    Add hidden attribute to Item class

--- items.py
+++ items.py
@@ -1,9 +1,10 @@
 class Item:
-    def __init__(self, name, description, location, position):
+    def __init__(self, name, description, location, position, hidden=False):
         self.name = name
         self.description = description
         self.location = location
         self.position = position
+        self.hidden = hidden
 
     def __repr__(self):
         return f'Item | {self.name}'
